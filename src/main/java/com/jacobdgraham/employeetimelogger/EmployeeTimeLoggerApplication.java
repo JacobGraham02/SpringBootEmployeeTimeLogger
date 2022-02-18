4e62c68661bae829a9d308c859efa32ad1fe225e
Jacob Graham <jakegraham54@gmail.com>
2022-02-18 16:27:27 -0500

    Created default spring boot file structure with dependencies for security, working with both relational and nosql databases, integrating with Azure, and doing a hot reboot when a change is detected in the source code

---
+++ src/main/java/com/jacobdgraham/employeetimelogger/EmployeeTimeLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.jacobdgraham.employeetimelogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTimeLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeTimeLoggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jacobdgraham/employeetimelogger/EmployeeTimeLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobdgraham.employeetimelogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTimeLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
